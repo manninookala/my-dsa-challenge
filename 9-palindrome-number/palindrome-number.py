30ca1bce19d94e3bf0027f40f40a9baac91f045b
Manni Megna Nookala <89378476+manninookala@users.noreply.github.com>
2026-01-22 18:49:58 +0530

    Time: 16 ms (14.25%) | Memory: 19.3 MB (15.50%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x <= 9 and x > 0:
+            return True
+
+        arr = []
+        while x != 0:
+            temp = x%10
+            x = x//10
+            arr.append(temp)
+            if x <= 9:
+                arr.append(x)
+                break
+        n=len(arr)
+        limit = n//2
+        for i in range(limit):
+            if arr[i] == arr[n-1-i]:
+                continue
+            else :
+                return False
+        return True
+        
+        
